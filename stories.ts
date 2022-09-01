d9993ff1de6ab3847c65e554a99ea5d91c051a64
Sebastien Ringrose <seb@codeworks.me>
2022-09-01 14:57:42 +0100

    fix: refactor away from Deno.readDirSync

--- stories.ts
+++ stories.ts
@@ -2,42 +2,31 @@ import * as Peko from "https://deno.land/x/peko@v0.5/mod.ts"
 import { fromFileUrl } from "https://deno.land/std@0.150.0/path/mod.ts"
 import MarkdownIt from "https://esm.sh/markdown-it@13.0.1"
 
-const decoder = new TextDecoder()
 const md = new MarkdownIt()
 const cache = new Peko.ResponseCache()
-
 const htmlDoc = await Deno.readTextFile("./public/index.html")
+const storyRoutes: Peko.Route[] = []
 
-const storyData = Array.from(
-  Deno.readDirSync(
-    fromFileUrl(new URL("./stories", import.meta.url))
-  )
-)
-  .filter(file => file.name.includes(".md"))
-  .map(storyFile => {
-    const content = decoder.decode(
-      Deno.readFileSync(
-        fromFileUrl(new URL(`./stories/${storyFile.name}`, import.meta.url))
-      )
+const storiesPath = fromFileUrl(new URL("./stories", import.meta.url))
+
+for await (const dirEntry of Deno.readDir(storiesPath)) {
+  if (dirEntry.name.includes(".md")) {
+    const content = await Deno.readTextFile(
+      fromFileUrl(new URL(`./stories/${dirEntry.name}`, import.meta.url))
     )
 
-    return { 
-      ...storyFile,
-      url: `/stories/${storyFile.name.replace(".md", "")}`, 
-      html: md.render((content))
-    }
-  })
-
-const storyRoutes = storyData.map(story => ({
-  route: story.url,
-  middleware: Peko.cacher(cache),
-  handler: Peko.ssrHandler({
-    render: () => htmlDoc.replace(
-      /(?<=<main(.)*>)(.|\n)*?(?=<\/main>)/,
-      story.html
-    )
-  })
-}))
+    storyRoutes.push({
+      route: `/stories/${dirEntry.name.replace(".md", "")}`,
+      middleware: Peko.cacher(cache),
+      handler: Peko.ssrHandler({
+        render: () => htmlDoc.replace(
+          /(?<=<main(.)*>)(.|\n)*?(?=<\/main>)/,
+          md.render((content))
+        )
+      })
+    })
+  }
+}
 
 storyRoutes.push({
   route: "/stories",
@@ -47,10 +36,10 @@ storyRoutes.push({
       /(?<=<main(.)*>)(.|\n)*?(?=<\/main>)/,
       `<h1>Stories</h1>
       <ul class="story-list">
-        ${storyData.map(story => `
+        ${storyRoutes.map(sRoute => `
           <li class="story-list-item">
-            <a href="${story.url}">
-              <h2>${story.name.toUpperCase()}</h2>
+            <a href="${sRoute.route}">
+              <h2>${sRoute.route.slice(sRoute.route.lastIndexOf("/")+1).toUpperCase()}</h2>
             </a>
           </li>
         `)}

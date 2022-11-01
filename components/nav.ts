c6cf29c5a134b4079f622656a7310420e44379ee
Sebastien Ringrose <seb@codeworks.me>
2022-11-01 11:38:26 +0000

    feat: init menu

--- components/nav.ts
+++ components/nav.ts
@@ -1,27 +1,61 @@
 customElements.define('nav-menu', class NavMenu extends HTMLElement {
-  open = false;
-  
+  home = document.querySelector("#info")
+  nextSection = document.querySelector("#showcase")
+
+  initWidth = this.style.width
+  initHeight = this.style.height
+  initRadius = this.style.borderRadius
+  initTransform = this.style.transform
+  initTop = this.style.top
+  initRight = this.style.right
+  initColor = this.style.color
+  initBg = this.style.backgroundColor
+
+  canSeeShowcase = false
+
   constructor() {
     super();
-
-    this.addEventListener('click', this.toggleOpen);
-    this.style.transition = 'all 40ms ease-in-out';
-
-    console.log("constructed!", this)
+    globalThis.document.querySelector("#nav-button")!.addEventListener('click', () => {
+      this.toggleOpen()
+    });
   }
 
   connectedCallback() {
-    console.log('connected.')
+    const observer = new IntersectionObserver(e => {
+      const showcaseInView = e[0].isIntersecting
+      if (showcaseInView && showcaseInView !== this.canSeeShowcase) {
+        this.canSeeShowcase = true
+        this.moveToBody()
+      }
+      if (!showcaseInView && showcaseInView !== this.canSeeShowcase) {
+        this.canSeeShowcase = false
+        this.moveBackHome()
+      }
+    }, {
+      root: null, // document body
+      rootMargin: "0px" // viewport bounds
+    });
+      
+    observer.observe(this.nextSection!)
+  }
+
+  moveToBody() {
+    globalThis.document.body.insertBefore(this, globalThis.document.body.firstChild)
+  }
+
+  moveBackHome() {
+    this.home!.insertBefore(this, this.home!.firstChild)
   }
 
   toggleOpen() {
-    console.log('clicked', this.open)
-    this.open = !this.open
+    this.classList.toggle("isOpen");
+    
+    if (this.classList.contains("isOpen")) {
+      globalThis.document.querySelector("#nav-button")!.textContent = "X";
+    } else {
+      globalThis.document.querySelector("#nav-button")!.textContent = "+";
+    }
   }
 }, {
   extends: "nav"
 });
-
-console.log("im=here")
-
-document.querySelector("nav")?.setAttribute("is", "nav-menu")

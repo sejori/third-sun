ba283f7014e157ef1cb9a4b974aef76cd0d6b734
Sebastien Ringrose <seb@codeworks.me>
2022-11-02 12:36:34 +0000

    🤘🤘🤘

--- components/nav.ts
+++ components/nav.ts
@@ -2,6 +2,7 @@ customElements.define('nav-menu', class NavMenu extends HTMLElement {
   home = this.parentElement
   ogHeight = this.style.height
   ogWidth = this.style.width
+  open = false
 
   // used to throlle calls from IntersectionObserver
   // and relocate if menu opened not at scroll position
@@ -46,7 +47,9 @@ customElements.define('nav-menu', class NavMenu extends HTMLElement {
   }
 
   async toggleOpen() {
+    this.open = !this.open
     this.classList.toggle("isOpen");
+
     if (!this.onBody) this.home?.scrollIntoView({
       behavior: "smooth"
     })
@@ -58,8 +61,10 @@ customElements.define('nav-menu', class NavMenu extends HTMLElement {
     } else {
       globalThis.document.querySelector("#nav-button")!.textContent = "+"
       await new Promise(res => setTimeout(res, 600))
-      this.style.height = this.ogHeight
-      this.style.width = this.ogWidth
+      if (!this.open) {
+        this.style.height = this.ogHeight
+        this.style.width = this.ogWidth
+      }
     }
   }
 }, {

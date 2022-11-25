e39b4dc623eb915b8dc194220fbb9fcb321b3620
Sebastien Ringrose <seb@codeworks.me>
2022-11-25 09:18:57 +0000

    feat: restrict resolutions

--- routes/images.ts
+++ routes/images.ts
@@ -4,6 +4,7 @@ import { fromFileUrl } from "fromFileUrl"
 import { instantiate } from "../lib/rs_lib.generated.js";
 
 const { resize_image } = await instantiate();
+const ALLOWED_RESOLUTIONS = [400,600,800,1000,1200,1400,1600,1800,2000]
 
 const cache = new Peko.ResponseCache()
 const files = await recursiveReaddir(fromFileUrl(new URL("../static/images", import.meta.url)))
@@ -17,10 +18,14 @@ export default files.map((file): Peko.Route => {
     handler: async (ctx) => await Peko.staticHandler(new URL(`../${fileRoute}`, import.meta.url), {
       transform: (contents) => {
         const params = new URL(ctx.request.url).searchParams
-        const resolution = params.get("resolution")?.split("x")
-        if (!resolution || resolution.length !== 2) return contents
+        const [widthStr, heightStr]  = params.get("resolution")?.split("x") || []
+        const width = Number(widthStr), height = Number(heightStr)
 
-        return resize_image(contents, Number(resolution[0]), Number(resolution[1]))
+        if (
+          !ALLOWED_RESOLUTIONS.includes(width) || !ALLOWED_RESOLUTIONS.includes(height)
+        ) return contents
+
+        return resize_image(contents, width, height)
       },
       headers: new Headers({
         "Cache-Control": "max-age=600, stale-while-revalidate=86400"

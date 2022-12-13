0e0dfcccfcbe4681056db21e79ab5df34af3e832
Sebastien Ringrose <seb@codeworks.me>
2022-12-13 11:57:28 +0000

    experimenting

--- handlers/resizable-image.ts
+++ handlers/resizable-image.ts
@@ -6,28 +6,35 @@ import { instantiate } from "../lib/tjw_rust.generated.js"
 
 import { IMG_RESOLUTIONS } from "../components/config.ts"
 
-const { alloc, resize } = await instantiate()
+const { resize } = await instantiate()
 
 export const resizableImage =  (fileRoute: string) => async (ctx: RequestContext) => await staticHandler(new URL(`../${fileRoute}`, import.meta.url), {
   transform: (contents) => {
-    const params = new URL(ctx.request.url).searchParams
-    const res  = IMG_RESOLUTIONS.get(params.get("res"))
+    const params = new URL(ctx.request.url).searchParams;
+    const res = IMG_RESOLUTIONS.get(params.get("res"));
 
     if (!res) return contents
 
-    // create pointer to rust memory buffer of content size
-    const ptr = alloc(contents.length)
+    const memory = new WebAssembly.Memory({
+      initial: 10,
+      maximum: 100,
+    });
 
-    // copy content buffer into rust memory and store ref?
-    const buffer = new Uint8Array(contents, ptr, contents.length);
+    // copy content buffer into rust memory buffer
+    const buffer = new Uint8Array(memory.buffer, undefined, contents.length);
+    for (let i = 0; i < contents.length; i++) {
+      buffer[i] = contents[i];
+    }
+
     console.log(buffer.length);
 
-    // perform resize 
-    resize(ptr, contents.length, res, res)
+    // perform resize with copy out
+    const newLength = resize(memory, res, res);
+    const newBuffer = new Uint8Array(memory.buffer, undefined, newLength);
 
-    console.log(buffer.length)
+    console.log(newBuffer.length);
 
-    return buffer;
+    return newBuffer;
   },
   headers: new Headers({
     "Cache-Control": "max-age=600, stale-while-revalidate=86400"
--- tjw_rust/src/lib.rs
+++ tjw_rust/src/lib.rs
@@ -1,10 +1,13 @@
 extern crate image;
 
 use wasm_bindgen::prelude::*;
+use js_sys::{WebAssembly, Uint8Array};
 use std::io;
 use image::io::{Reader};
 use image::{guess_format};
 use image::imageops::FilterType;
+
+// https://wasmbyexample.dev/examples/webassembly-linear-memory/webassembly-linear-memory.rust.en-us.html
 
 #[wasm_bindgen]
 extern "C" {
@@ -15,131 +18,34 @@ extern "C" {
   fn log_u32(a: u32);
 }
 
-/// Allocate memory into the module's linear memory
-/// and return the offset to the start of the block.
 #[wasm_bindgen]
-pub fn alloc(len: usize) -> *mut u8 {
-    // create a new mutable buffer with capacity `len`
-    let mut buf = Vec::with_capacity(len);
-    // take a mutable pointer to the buffer
-    let ptr = buf.as_mut_ptr();
-    // take ownership of the memory block and
-    // ensure that its destructor is not
-    // called when the object goes out of scope
-    // at the end of the function
-    std::mem::forget(buf);
-    // return the pointer so the runtime
-    // can write data at this offset
-    return ptr;
-}
-
-/// Given a pointer to the start of a byte array and
-/// its length, return the sum of its elements.
-#[wasm_bindgen]
-pub unsafe fn resize(ptr: *mut u8, len: usize, width: u32, height: u32) -> usize {
+pub unsafe fn resize(memory: *mut WebAssembly::Memory, width: u32, height: u32) -> usize {
   // create a Vec<u8> from the pointer to the
   // linear memory and the length
-  let mut buffer = Vec::from_raw_parts(ptr, len, len);
+  let js_buf = memory.as_ref().unwrap().buffer();
+  let array = Uint8Array::new(&js_buf);
+  let mut buffer: Vec<u8> = array.to_vec();
 
   // guess data format
   let format = guess_format(&buffer).unwrap_or(image::ImageFormat::Png);
 
   // decode data into DynamicImage
-  let full = Reader::new(io::Cursor::new(&buffer))
+  let image = Reader::new(io::Cursor::new(&buffer))
     .with_guessed_format()
     .unwrap()
     .decode()
     .unwrap();
 
+    log("here");
+
   // log_u32(width);
   // log_u32(height);
 
-  // resize DynamicImage
-  let resized = full.resize(width, height, FilterType::Triangle);
+  // // resize DynamicImage
+  let resized = image.resize(width, height, FilterType::Triangle);
 
-  // write to data
+  // // write to data
   resized.write_to(&mut io::Cursor::new(&mut buffer), format).expect("REASON");
 
   return buffer.len();
 }
-
-// #[wasm_bindgen] 
-// struct ResizableImage{ buffer: Vec<u8> }
-// #[wasm_bindgen] 
-// impl ResizableImage {
-//   #[wasm_bindgen(constructor)] 
-//   pub fn new(self) -> ResizableImage {
-//     self.buffer = Vec::new();
-//     return self;
-//   }
-  
-//   fn get_buffer(&mut self) -> &Vec<u8> {
-//     return &mut self.buffer;
-//   }
-
-//   fn resize_buffer(&mut self, width: u32, height: u32) -> &Vec<u8> {
-//     // guess data format
-//     let format = guess_format(&self.buffer).unwrap_or(image::ImageFormat::Png);
-
-//     // decode data into DynamicImage
-//     let full = Reader::new(io::Cursor::new(&self.buffer))
-//       .with_guessed_format()
-//       .unwrap()
-//       .decode()
-//       .unwrap();
-
-//     // log_u32(width);
-//     // log_u32(height);
-
-//     // resize DynamicImage
-//     let resized = full.resize(width, height, FilterType::Triangle);
-
-//     // write to data
-//     resized.write_to(&mut io::Cursor::new(&mut self.buffer), format).expect("REASON");
-
-//     return &self.buffer;
-//   }
-// }
-
-// #[wasm_bindgen] 
-// pub fn resize_image_buffer(buffer: &JsValue, width: u32, height: u32) -> Vec<u8> {
-//   // guess data format
-//   let format = guess_format(&buffer).unwrap_or(image::ImageFormat::Png);
-
-//   // decode data into DynamicImage
-//   let full = Reader::new(io::Cursor::new(&buffer))
-//     .with_guessed_format()
-//     .unwrap()
-//     .decode()
-//     .unwrap();
-
-//   // log_u32(width);
-//   // log_u32(height);
-
-//   // resize DynamicImage
-//   let resized = full.resize(width, height, FilterType::Triangle);
-
-//   // write to data
-//   resized.write_to(&mut io::Cursor::new(buffer), format).expect("REASON");
-
-//   return buffer;
-// }
-
-// let image = catch_unwind(|| {
-//   Reader::new(io::Cursor::new(&data))
-//     .with_guessed_format()
-//     .expect("REASON")
-//     .decode()
-//     .expect("REASON");
-// });
-
-// #[cfg(test)]
-// mod tests {
-//   use super::*;
-
-//   #[test]
-//   fn it_works() {
-//     let result = add(1, 2);
-//     assert_eq!(result, 3);
-//   }
-// }

e8ee6c440bf572fb15e36869223e355fbda70931
Sebastien Ringrose <seb@codeworks.me>
2023-03-01 12:28:49 +0000

    feat: fileWatch event test

--- server.ts
+++ server.ts
@@ -16,3 +16,14 @@ server.use(async (_, next) => {
 server.use(router)
 
 server.listen(3000) // say hello Rabbit boi .b,b!
+
+// hot-reload events
+if (!Deno.env.get("DENO_DEPLOYMENT_ID")) {
+  const watchTarget = new EventTarget()
+
+  server.addRoute("/devSocket", Peko.sseHandler(watchTarget))
+  const watcher = Deno.watchFs(new URL("./public", import.meta.url).pathname)
+  for await (const event of watcher) {
+    watchTarget.dispatchEvent(new CustomEvent("send", { detail: event }))
+  }
+}

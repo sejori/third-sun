8caf3ef591ba086c96e7bc9cef3ead5285f5449d
Sebastien Ringrose <seb@codeworks.me>
2023-03-24 21:51:01 +0000

    esm.sh deps

--- utils/react.ts
+++ utils/react.ts
@@ -1,7 +1,7 @@
-import React from "npm:react@18.2.0"
+import React from "https://esm.sh/react@18.2.0?dev"
 import htm from "https://esm.sh/htm@3.1.1"
 export { 
   renderToString,
   renderToReadableStream 
-} from "npm:react-dom@18.2.0/server"
+} from "https://esm.sh/react-dom@18.2.0/server"
 export const html = htm.bind(React.createElement)

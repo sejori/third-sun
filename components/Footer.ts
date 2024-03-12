09ff1f7c172375030339f3e8fc04e4b30dcfe972
IIISun <112644676+IIISun@users.noreply.github.com>
2024-03-12 10:54:40 +0000

    Update Footer.ts

--- components/Footer.ts
+++ components/Footer.ts
@@ -4,8 +4,8 @@ const Footer = () => html`
   <footer class="parallax__group container justify-end">
     <div class="align-center">
       <div class="footer-text-big">&nbsp;</div>
-      <h2>Do you want a demonic website?</h2>
-      <h3><a href="">Summon satanic forces</a></h3>
+      <h2>Do you want your own website?</h2>
+      <h3><a href="">Summon incredible visuals!</a></h3>
       <p>
         Made with ❤️‍🔥 by <a href="https://thesebsite.com">seb</a> & <a href="https://iiisun.art">iiisun</a>.
       </p>
